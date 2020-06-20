493056c23b14fb982b572c553000b918cea90740
punam kwatra <punam2005@hotmail.com>
2020-06-20 08:38:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,7 +44,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["wow","I",]
+  array = ["wow","I","am", "really","learning","arrays!"]
+  
   
 end
 
