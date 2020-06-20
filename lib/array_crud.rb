1a2f13f25be8d58d1f3a7b2bbf4c30e50caf561f
punam kwatra <punam2005@hotmail.com>
2020-06-20 08:11:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,38 @@
-def create_an_empty_array
+def create_an_empty_array  
+  []
   
-end
+  end
 
-def create_an_array
+def create_an_array 
+  ["white","blue", "red", "green",]
+  
   
 end
 
 def add_element_to_end_of_array(array, element)
+  add_element_to_end_of_array = ["white","blue"]
+  .push("arrays!")
   
+  
+  
+ 
 end
 
 def add_element_to_start_of_array(array, element)
+  add_element_to_start_of_array = ["beauty","luxury"]
+  .unshift("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array = array
+  array= remove_element_from_end_of_array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  remove_element_from_start_of_array = array
+  array = remove_element_from_start_of_array.shift
   
 end
 
